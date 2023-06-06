cca1b8f32eabd850975dbd1b763cc217aeea0885
DIPAYAN GHOSE <dipayan.ghose@pfizer.com>
2023-06-06 17:54:49 +0530

    0.0.10

--- ios/Plugin/docxreader.swift
+++ ios/Plugin/docxreader.swift
@@ -4,14 +4,14 @@ import SNDocx
 @objc public class docxreader: NSObject {
     @objc public func echo(_ value: String) -> String {
         print(value)
-        return value
+        guard let docxURL = URL(string: value) else { return "docx URL issue" }
+        let extractedText = extractTextFromDocx(docxURL: docxURL)
+        return extractedText
     }
     
     @objc public func getTextFromDocxFile(_ value: String) -> String {
         print(value)
-        guard let docxURL = URL(string: value) else { return "docx URL issue" }
-        let extractedText = extractTextFromDocx(docxURL: docxURL)
-        return extractedText
+        return value
     }
     
     private func extractTextFromDocx(docxURL: URL) -> String{
